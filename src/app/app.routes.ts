f6dd4fae9f16b591d375f08864132dd784f61703
kenrick <kenrickdehli@gmail.com>
2024-04-25 22:06:26 +0200

    added navbar, color switch, sign-up component, routing, a welcome route + component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: WelcomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent},
+];
